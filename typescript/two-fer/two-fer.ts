56854ba04d1f5ea7cd7c2f8712a56a89e08478b7
John M <neshamon@protonmail.com>
2022-01-23 23:26:07 -0600

    Make name parameter optional

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,8 +1,5 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
 
-export function twoFer(name): string {
+export function twoFer(name?: string): string {
   if(name){
     return `One for ${name}, one for me`
   } else {
