a36c63bf656cc4d836b4737431f9b7af790082f7
John M <neshamon@protonmail.com>
2022-02-02 10:17:15 -0600

    Unfinished

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,18 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colorArray) {
+  const colorMap: any = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  let firstValue: any = colorMap[`${colorArray[1]}`];
+  let secondValue: any = colorMap[`${colorArray[2]}`];
+  return firstValue + secondValue
 }
